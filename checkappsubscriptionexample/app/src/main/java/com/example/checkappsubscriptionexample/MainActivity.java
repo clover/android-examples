19461b94272bafb5f5e4cc9b72246c3b8518c5ba
jshendwg <jiong@clover.com>
2014-07-01 19:16:25 -0700

    cleaning up

--- checkappsubscriptionexample/app/src/main/java/com/example/checkappsubscriptionexample/MainActivity.java
+++ checkappsubscriptionexample/app/src/main/java/com/example/checkappsubscriptionexample/MainActivity.java
@@ -1,17 +1,16 @@
-package com.example.zachsubscriptionapp.app;
+package com.example.checkappsubscriptionexample;
 
 import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.View;
 import android.widget.Button;
-import android.os.RemoteException;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.clover.sdk.internal.util.Strings;
 import com.clover.sdk.util.CloverAccount;
 import com.clover.sdk.v1.BindingException;
