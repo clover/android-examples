54dcce2617cced8ac99ec05557a32df594b2bb4f
Mike Maietta <mmaietta@outlook.com>
2015-10-15 11:03:39 -0700

    adding barcode util to help decode for Mobile Printer barcodes

--- barcodereceiver/app/src/main/java/com/clover/example/barcodereceiver/app/MainActivity.java
+++ barcodereceiver/app/src/main/java/com/clover/example/barcodereceiver/app/MainActivity.java
@@ -6,53 +6,57 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-  private final BarcodeReceiver barcodeReceiver = new BarcodeReceiver();
-  private TextView mTextView;
+    public static final String BARCODE_BROADCAST = "com.clover.stripes.BarcodeBroadcast";
+    private final BarcodeReceiver barcodeReceiver = new BarcodeReceiver();
+    private TextView mTextView;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    mTextView = (TextView) findViewById(R.id.barcode);
-  }
-
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    registerBarcodeScanner();
-  }
-
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    unregisterReceiver(barcodeReceiver);
-  }
-
-  private void registerBarcodeScanner() {
-    registerReceiver(barcodeReceiver, new IntentFilter("com.clover.stripes.BarcodeBroadcast"));
-  }
-
-  private void unregisterBarcodeScanner() {
-    unregisterReceiver(barcodeReceiver);
-  }
-
-  private class BarcodeReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent) {
-      String action = intent.getAction();
-      if (action.equals("com.clover.stripes.BarcodeBroadcast")) {
-        String barcode = intent.getStringExtra("Barcode");
-        if (barcode != null) {
-           mTextView.setText("Barcode scanned : " + barcode);
-        }
-      }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.barcode);
     }
-  }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerBarcodeScanner();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterBarcodeScanner();
+    }
+
+    private void registerBarcodeScanner() {
+        registerReceiver(barcodeReceiver, new IntentFilter(BARCODE_BROADCAST));
+    }
+
+    private void unregisterBarcodeScanner() {
+        unregisterReceiver(barcodeReceiver);
+    }
+
+    private class BarcodeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(BARCODE_BROADCAST)) {
+                String barcode = intent.getStringExtra("Barcode");
+                if (barcode != null) {
+                    // Clover OrderIds are sometimes encoded to fit in the barcode (Mobile Printer especially)
+                    // If it is not the full OrderId, then it will be a prefix that you can filter by.
+                    barcode = BarcodeIdUtil.safeBase64toBase32(barcode);
+                    mTextView.setText("Barcode scanned : " + barcode);
+                }
+            }
+        }
+    }
 }
+
