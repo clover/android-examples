6411baac04490f46a79df4fba84a8deb9d5fff9e
Mike Maietta <mmaietta@outlook.com>
2015-11-18 09:45:38 -0800

    fixing layout and adding toggle to display difference between mini/mobile receipt barcodes and other barcodes

--- barcodereceiver/app/src/main/java/com/clover/example/barcodereceiver/app/MainActivity.java
+++ barcodereceiver/app/src/main/java/com/clover/example/barcodereceiver/app/MainActivity.java
@@ -6,14 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Base64;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
     public static final String BARCODE_BROADCAST = "com.clover.BarcodeBroadcast";
     private final BarcodeReceiver barcodeReceiver = new BarcodeReceiver();
     private TextView mTextView;
+    private CheckBox checkBox;
+    private String scannedBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.barcode);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -43,6 +47,24 @@ public class MainActivity extends Activity {
         unregisterReceiver(barcodeReceiver);
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        displayBarcode();
+    }
+
+    private void displayBarcode() {
+        if (scannedBarcode == null)
+            return;
+
+        String barcode = scannedBarcode;
+        // Clover OrderIds are sometimes encoded to fit in the barcode (Mini/Mobile Printer specifically)
+        // If it is not the full OrderId, then it will be a prefix that you can filter by.
+        if (checkBox.isChecked())
+            barcode = BarcodeIdUtil.safeBase64toBase32(barcode);
+
+        mTextView.setText(getString(R.string.barcode_scanned,barcode));
+    }
+
     private class BarcodeReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -50,10 +72,8 @@ public class MainActivity extends Activity {
             if (action.equals(BARCODE_BROADCAST)) {
                 String barcode = intent.getStringExtra("Barcode");
                 if (barcode != null) {
-                    // Clover OrderIds are sometimes encoded to fit in the barcode (Mobile Printer especially)
-                    // If it is not the full OrderId, then it will be a prefix that you can filter by.
-                    barcode = BarcodeIdUtil.safeBase64toBase32(barcode);
-                    mTextView.setText("Barcode scanned : " + barcode);
+                    scannedBarcode = barcode;
+                    displayBarcode();
                 }
             }
         }
