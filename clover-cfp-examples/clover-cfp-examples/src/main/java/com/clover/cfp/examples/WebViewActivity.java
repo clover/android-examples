54726a494e4b3230c9a5d2514ae6496f795cb7f8
rachelantion <rachel.antion@clover.com>
2018-05-02 10:25:00 -0600

    added copyright details

--- clover-cfp-examples/clover-cfp-examples/src/main/java/com/clover/cfp/examples/WebViewActivity.java
+++ clover-cfp-examples/clover-cfp-examples/src/main/java/com/clover/cfp/examples/WebViewActivity.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright (C) 2018 Clover Network, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.clover.cfp.examples;
 
-/**
- * Created by blakewilliams on 3/16/17.
- */
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -26,7 +39,7 @@ public class WebViewActivity extends CloverCFPActivity {
     setContentView(R.layout.activity_web_view);
 
     webView = (WebView)findViewById(R.id.webView);
-    AndroidBug5497Workaround.assistActivity(this);
+//    AndroidBug5497Workaround.assistActivity(this);
 
   }
 
